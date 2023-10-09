9b26c3f4fc855f7780b75a3786c37a5472ce2dc7
lashikala hettiarachchi <nadileshan1995@gmail.com>
2023-10-09 10:39:33 +0100

    aa2

---
+++ js/translations.js
@@ -0,0 +1,249 @@
+const p1_sp = "¡Bienvenido a la comunidad de Luminariums, un lugar donde la brillantez irradia! En esta guía, exploraremos lo que implica ser un Luminarium, las cualidades únicas que distinguen a los Luminariums y las ventajas de formar parte de esta comunidad extraordinaria."
+const p1_en = "Welcome to the Luminarium, a place where brilliance radiates! In this guide, we'll explore what it means to be a Luminarium, the unique qualities that set Luminariums apart, and the benefits of being part of this extraordinary community."
+
+const p2_sp = "Qué es un Luminarium?"
+const p2_en = "What is a Luminarium?"
+
+const p3_sp = "Un Luminarium no es solo un título; es un reconocimiento de un talento extraordinario y pensamiento reflexivo. Los Luminariums son individuos que se destacan en su capacidad para pensar críticamente, resolver problemas complejos y contribuir de manera significativa en diversos campos.Los Luminariums poseen varias cualidades destacadas que los distinguen:"
+const p3_en = "A Luminarium is not just a title; It is a recognition of extraordinary talent and thoughtful thinking. Luminariums are individuals who excel in their ability to think critically, solve complex problems, and contribute meaningfully in various fields. Luminariums have several outstanding qualities that distinguish them:"
+
+const Q_A1_EN = "Brilliance: Luminariums are known for their exceptional intelligence and creativity. They excel in their respective fields and continually seek innovative solutions to challenges."
+const Q_A1_SP = "Son brillantes: Los Luminariums son conocidos por su inteligencia excepcional y creatividad. Sobresalen en sus respectivos campos y buscan continuamente soluciones innovadoras a los desafíos."
+const Q_A2_EN = "Reflection: Luminariums are deep thinkers who approach problems with empathy and a holistic perspective. They consider the broader impact of their actions on society."
+const Q_A2_SP = "Reflexión: Los Luminariums son pensadores profundos que abordan los problemas con empatía y una perspectiva holística. Consideran el impacto más amplio de sus acciones en la sociedad."
+const Q_A3_EN = "Innovation: Luminariums are innovative at heart. They are always pushing boundaries and exploring new horizons in their areas of expertise."
+const Q_A3_SP = "Innovación: Los Luminariums son innovadores de corazón. Siempre están desafiando límites y explorando nuevos horizontes en sus áreas de experiencia."
+const Q_A4_EN = "Problem Solving Skills: Luminariums have a knack for tackling complex problems. They break down challenges and develop elegant solutions.and. Collaboration: Luminariums thrive in collaborative environments. They value teamwork, which makes them effective leaders and contributors in group efforts."
+const Q_A4_SP = "Destreza en la Resolución de Problemas: Los Luminariums tienen un don para abordar problemas complejos. Descomponen los desafíos y desarrollan soluciones elegantes."
+const Q_A5_EN = "Collaboration: Luminariums thrive in collaborative environments. They value teamwork, which makes them effective leaders and contributors in group efforts."
+const Q_A5_SP = "Colaboración: Los Luminariums prosperan en entornos colaborativos. Valoran el trabajo en equipo, lo que los convierte en líderes eficaces y contribuyentes en esfuerzos grupales."
+
+
+const para3_EN = "Joining the Luminarium comes with a number of benefits:"
+const para3_SP = "Unirse a la comunidad de Luminariums conlleva una serie de beneficios:"
+
+// Q1 set
+const Q1_A1_EN = "Networking: Connect with like-minded people who share your passion for innovation and excellence. The Luminariums are part of a global network of brilliant minds."
+const Q1_A1_sp = "Networking: Conecta con personas afines que comparten tu pasión por la innovación y la excelencia. Los Luminariums forman parte de una red global de mentes brillantes."
+const Q1_A2_EN = "Learning Opportunities: Access exclusive events, seminars and workshops designed to improve your skills and knowledge. Learn from other Luminariums and experts in various fields."
+const Q1_A2_sp = "Oportunidades de Aprendizaje: Accede a eventos exclusivos, seminarios y talleres diseñados para mejorar tus habilidades y conocimientos. Aprende de otros Luminariums y expertos en diversos campos."
+const Q1_A3_EN = "Recognition: Your achievements as Luminarium are celebrated and recognized within the community. You gain visibility for your work and contributions."
+const Q1_A3_sp = "Reconocimiento: Tus logros como Luminarium se celebran y reconocen dentro de la comunidad. Ganas visibilidad por tu trabajo y contribuciones."
+const Q1_A4_EN = "Collaborative Projects: Collaborate on exciting projects with other Luminariums, fostering creativity and innovation in a supportive environment."
+const Q1_A4_sp = "Proyectos Colaborativos: Colabora en emocionantes proyectos con otros Luminariums, fomentando la creatividad y la innovación en un entorno de apoyo."
+const Q1_A5_EN = "Mentoring: Access mentoring opportunities with experienced Luminariums who can guide you on your path."
+const Q1_A5_sp = "Mentoría: Accede a oportunidades de mentoría con Luminariums experimentados que pueden guiarte en tu camino.."
+const Q1_A6_EN = "Impact Initiatives: Contribute to meaningful initiatives that seek to address some of the world's most pressing challenges."
+const Q1_A6_sp = "Iniciativas de Impacto:  Contribuye a iniciativas significativas que buscan abordar algunos de los desafíos más apremiantes del mundo."
+
+const para_4_EN = "Becoming a Luminarium is an invitation to be part of an extraordinary community. If you believe you possess the qualities that define a Luminarium. We welcome those passionate about making a positive impact on the world and dedicated to the pursuit of a better future.In the Luminariums community, your ideas will inspire others to illuminate their own paths.At Luminarium, we value and respect the privacy of our members. All personal details and information shared in this community are kept strictly confidential."
+const para_4_SP = "Convertirse en un Luminarium es una invitación a ser parte de una comunidad extraordinaria. Si crees que posees las cualidades que definen a un Luminarium. Damos la bienvenida a aquellos apasionados por hacer un impacto positivo en el mundo y dedicados a la búsqueda de un un mejor futuro.En la comunidad de Luminariums, tus ideas inspirarán a otros a iluminar sus propios caminos. En Luminarium, valoramos y respetamos la privacidad de nuestros miembros. Todos los datos personales y la información compartida en esta comunidad se mantienen estrictamente confidenciales."
+
+const ab_St_EN = "Scale Your Startup Globally Compete against the world's best and win 1 Million USD."
+const ab_St_SP = "Eleva tu StartUp a Escala Global Participa contra los mejores del mundo y Gánate 1 Millón de USD"
+
+const skilset_EN = "ELIGIBILITY";
+const skilset_SP = "Categorias";
+
+const ski1_EN = "Information technology";
+const ski1_SP = "Tecnologías de la Información "
+const ski2_EN = "Artificial intelligence";
+const ski2_SP = "Inteligencia Artificial"
+const ski3_EN = "Blockchain";
+const ski3_SP = "Blockchain";
+const ski4_EN = "Internet of Things (IoT)";
+const ski4_SP = "Internet de las Cosas (IoT) ";
+const ski5_EN = "Virtual and Augmented Reality";
+const ski5_SP = "Realidad Virtual y Aumentada ";
+const ski6_EN = "Renewable Energy and Sustainability";
+const ski6_SP = "Energías Renovables y Sostenibilidad ";
+const ski7_EN = "Biotechnology and Health Sciences";
+const ski7_SP = "Biotecnología y Ciencias de la Salud ";
+const ski8_EN = "Robotics and Automation";
+const ski8_SP = "Robótica y Automatización ";
+const ski9_EN = "Financial Technology";
+const ski9_SP = "Tecnología Financiera";
+const ski10_EN = "Technology and entertainment";
+const ski10_SP = "Tecnología y entretenimiento";
+
+const lastP1_EN = "The future awaits, and it starts with you. ";
+const lastP1_SP = "Demos forma al futuro, donde la innovación no conoce límites.";
+
+const lastP2_EN = "Join the Luminarium community today and be part of a global network of visionary thinkers and doers.";
+const lastP2_SP = "Conviértete en un Luminarium y juntos iluminemos el camino hacia un mañana más brillante.El futuro te espera y comienza contigo. Únase a la comunidad Luminarium hoy y sea parte de una red global de pensadores y hacedores visionarios.";
+
+const lastP3_EN = "Together, we'll light up the world.";
+const lastP3_SP = "Juntos, iluminaremos el mundo.";
+
+const greeting = document.getElementById('Hd_lu');
+const languageSelect = document.getElementById('language-select');
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+    greeting.textContent = 'Sobre Luminario';
+  } else if (languageSelect.value === 'en') {
+    greeting.textContent = 'About Luminarium';
+  }
+});
+
+// 
+const para_1 = document.getElementById('first_para');
+
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+    para_1.textContent = p1_sp ;
+  } else if (languageSelect.value === 'en') {
+    para_1.textContent = p1_en;
+  }
+});
+// 
+const whatisLu = document.getElementById('whatisLu');
+
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+    whatisLu.textContent = p2_sp ;
+  } else if (languageSelect.value === 'en') {
+    whatisLu.textContent = p2_en;
+  }
+});
+
+// 
+const para_3 = document.getElementById('sec_para');
+
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+    para_3.textContent = p3_sp ;
+  } else if (languageSelect.value === 'en') {
+    para_3.textContent = p3_en;
+  }
+});
+
+const Qa = document.getElementById('Q_A');
+
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+    Q_A.textContent = Q_A1_SP ;
+  } else if (languageSelect.value === 'en') {
+    Q_A.textContent = Q_A1_EN;
+  }
+});
+
+
+// 
+
+const Q_B = document.getElementById('Q_B');
+const Q_C = document.getElementById('Q_C');
+const Q_D = document.getElementById('Q_D');
+const Q_E = document.getElementById('Q_E');
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+
+    Q_B.textContent = Q_A2_SP ;
+    Q_C.textContent = Q_A3_SP ;
+    Q_D.textContent = Q_A4_SP ; 
+    Q_E.textContent =  Q_A5_SP ; 
+  } else if (languageSelect.value === 'en') {
+    Q_A.textContent = Q_A1_EN;
+    Q_B.textContent = Q_A2_EN;
+    Q_C.textContent = Q_A3_EN;
+    Q_D.textContent = Q_A4_EN;  
+    Q_E.textContent = Q_A5_EN;
+  }
+});
+
+// 
+const third_para = document.getElementById('third_para');
+const Q2_A = document.getElementById('Q2_A');
+const Q2_B = document.getElementById('Q2_B');
+const Q2_C = document.getElementById('Q2_C');
+const Q2_D = document.getElementById('Q2_D');
+const Q2_E = document.getElementById('Q2_E');
+const Q2_F = document.getElementById('Q2_F');
+const forth_para = document.getElementById('forth_para');
+
+const st_para = document.getElementById('Ab_st');
+
+const SkiHD = document.getElementById('Hd_sk');
+const ski1 = document.getElementById('SK1');
+const ski2 = document.getElementById('SK2');
+const ski3 = document.getElementById('SK3');
+const ski4 = document.getElementById('SK4');
+const ski5 = document.getElementById('SK5');
+const ski6 = document.getElementById('SK6');
+const ski7 = document.getElementById('SK7');
+const ski8 = document.getElementById('SK8');
+const ski9 = document.getElementById('SK9');
+const ski10 = document.getElementById('SK10');
+
+const lastP1 = document.getElementById('strng1');
+const lastP2 = document.getElementById('last_p2');
+const lastP3 = document.getElementById('strng2');
+
+
+languageSelect.addEventListener('change', function() {
+  if (languageSelect.value === 'sp') {
+    third_para.textContent = para3_SP ;
+
+    Q2_A.textContent = Q1_A1_sp;
+    Q2_B.textContent = Q1_A2_sp;
+    Q2_C.textContent = Q1_A3_sp;
+    Q2_D.textContent = Q1_A4_sp;
+    Q2_E.textContent = Q1_A5_sp;
+    Q2_F.textContent = Q1_A6_sp;
+    
+    forth_para.textContent = para_4_SP;
+    st_para.textContent = ab_St_SP
+
+    SkiHD.textContent = skilset_SP;
+
+    ski1.textContent = ski1_SP;
+    ski2.textContent = ski2_SP;
+    ski3.textContent = ski3_SP;
+    ski4.textContent = ski4_SP;
+    ski5.textContent = ski5_SP;
+    ski6.textContent = ski6_SP;
+    ski7.textContent = ski7_SP;
+    ski8.textContent = ski8_SP;
+    ski9.textContent = ski9_SP;
+    ski10.textContent = ski10_SP;
+
+    lastP1.textContent = lastP1_SP;
+    lastP2.textContent = lastP2_SP;
+    lastP3.textContent = lastP3_SP;
+
+  } else if (languageSelect.value === 'en') {
+
+    third_para.textContent = para3_EN ;
+
+    Q2_A.textContent = Q1_A1_EN;
+    Q2_B.textContent = Q1_A2_EN;
+    Q2_C.textContent = Q1_A3_EN;
+    Q2_D.textContent = Q1_A4_EN;
+    Q2_E.textContent = Q1_A5_EN;
+    Q2_F.textContent = Q1_A6_EN;
+    
+    forth_para.textContent = para_4_EN;
+    st_para.textContent = ab_St_EN
+    SkiHD.textContent = skilset_EN
+    ski1.textContent = ski1_EN;
+    ski2.textContent = ski2_EN;
+    ski3.textContent = ski3_EN;
+    ski4.textContent = ski4_EN;
+    ski5.textContent = ski5_EN;
+    ski6.textContent = ski6_EN;
+    ski7.textContent = ski7_EN;
+    ski8.textContent = ski8_EN;
+    ski9.textContent = ski9_EN;
+    ski10.textContent = ski10_EN;
+
+    lastP1.textContent = lastP1_EN;
+    lastP2.textContent = lastP2_EN;
+    lastP3.textContent = lastP3_EN;
+    
+  }
+});
